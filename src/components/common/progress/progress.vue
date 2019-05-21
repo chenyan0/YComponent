4f8731243598ac55a7151740320b7bbd8068f0d2
陈燕 <870322979@qq.com>
2019-05-21 18:04:55 +0800

    update progress

--- src/components/common/progress/progress.vue
+++ src/components/common/progress/progress.vue
@@ -1,25 +1,46 @@
 <template>
-    <div class="v-progress">
+    <div class="v-progress" :class="classPrefix">
         <div class="v-progress-bar" v-if="type=='line'">
             <div class="v-progress-outer" :style="outerStyle" ref="outer">
                 <div class="v-progress-inner" :style="innerStyle">
     
                 </div>
             </div>
-            <span class="v-progress-text" v-if="showVal">{{percentage}}%</span>
+            <span class="v-progress-text" v-if="showText" :style="textStyle">
+                                    <template v-if="!status">{{percent}}%
+</template>
+
+<template v-else>
+    <font-awesome-icon :icon="['fas', 'check']" class="icon-clear" />
+</template>
+            </span>
         </div>
         <div class="v-progress-circle" v-else>
-            d
+            <svg id="circleProgress" ref="circleProgress" xmlns="http://www.w3.org/2000/svg">
+                <circle id="circle" ref="circle" cx="50%" cy="50%" r="40%" :stroke="strokeColor" stroke-width="10" stroke-dashoffset='310%' stroke-linecap="round"></circle>
+            </svg>
+            <svg id="circleProgress2" xmlns="http://www.w3.org/2000/svg">
+                <circle id="circle" cx="50%" cy="50%" r="40%" stroke="#e5e5ea" stroke-width="9" stroke-dashoffset='0'></circle>
+            </svg>
+            <span class="v-progress-circle-info">{{percent}}%</span>
         </div>
+<!-- <input id="range" ref="range" type="range" min="0" max="255" step="4" value="360" @input="drawCircle"> -->
     </div>
 </template>
 
 <script>
     export default {
         data() {
-            return {}
+            return {
+                percent: this.percentage,
+                rangeValue: 0,
+                rangeText: 0
+            }
         },
         props: {
+            classPrefix: {
+                type: String,
+            },
             width: {
                 type: String,
                 default: '200'
@@ -32,7 +53,11 @@
                 type: Number,
                 default: '100'
             },
-            showVal: {
+            step: {
+                type: Number,
+                default: 1
+            },
+            showText: {
                 type: Boolean,
                 default: true
             },
@@ -43,19 +68,50 @@
     
         },
         computed: {
-            innerStyle: function() {
+            status() {
+                return this.percent === 100 ? 'success' : null
+            },
+            textStyle() {
                 return {
-                    width: this.width * (this.percentage / 100),
-                    background: this.percentage === 100 ? '#52c41a' : this.strokeColor
+                    color: this.status == 'success' ? '#52c41a' : '#575757'
                 }
             },
-            outerStyle: function() {
+            innerStyle() {
+                const width = this.width;
+                const percentage = this.percent;
+                return {
+                    width: `${width*percentage/100}px`,
+                    background: this.percent === 100 ? '#52c41a' : this.strokeColor
+                }
+            },
+            outerStyle() {
                 return {
                     width: this.width + 'px',
-    
                 }
+            },
+    
+        },
+        methods: {
+            handleIncrease() {
+                if (this.percent < 100) {
+                    this.percent = this.percent + this.step
+                }
+            },
+            handleDecline() {
+                if (this.percent > 0) {
+                    this.percent = this.percent - this.step
+                }
+            },
+            drawCircle() {
+                const range = this.$refs.range
+                const circle = this.$refs.circle
+                const circleProcess = this.$refs.circleProcess
+                this.rangeValue = Number(range.value);
+                circle.setAttribute("stroke-dashoffset", (360 - this.rangeValue) + "%");
+                this.rangeText = Math.ceil(this.rangeValue / 360 * 100)
             }
-        }
+        },
+    
     
     }
 </script>
@@ -63,12 +119,12 @@
 <style lang="scss" scoped>
     $classes:'v-progress';
     .#{$classes} {
-        height: 20px;
         &-bar {
-            height: 10px;
+            display: table;
+            table-layout: fixed;
         }
         &-outer {
-            height: 100%;
+            height: 10px;
             border-radius: 10px;
             display: inline-block;
             background: #e5e5ea;
@@ -78,8 +134,58 @@
             border-radius: 10px;
         }
         &-text {
-            font-size: 14px;
+            font-size: 16px;
             margin-left: 10px;
+            vertical-align: middle;
+        }
+        &-circle {
+            width: 200px;
+            height: 200px;
+            position: relative;
+        }
+        &-circle-info {
+            position: absolute;
+            margin: auto;
+            top: 0;
+            left: 0;
+            right: 0;
+            bottom: 0;
+            text-align: center;
+            font-size: 16px;
+            line-height: 2.25;
+            height: 36px;
+        }
+        #circleProgress {
+            position: absolute;
+            
+            top: 0;
+            left: 0;
+            width: 200px;
+            height: 200px;
+            stroke-dasharray: 360%;
+            stroke-dashoffset: 360%;
+            fill: none;
+            -webkit-transform: rotate(-90deg);
+            -moz-transform: rotate(-90deg);
+            -ms-transform: rotate(-90deg);
+            -o-transform: rotate(-90deg);
+            transform: rotate(-90deg);
+            z-index: 2;
+        }
+        #circleProgress2 {
+            position: absolute;
+            top: 0;
+            left: 0;
+            width: 200px;
+            height: 200px;
+          stroke-dasharray: 360%;
+            stroke-dashoffset: 360%;
+            fill: none;
+            -webkit-transform: rotate(-90deg);
+            -moz-transform: rotate(-90deg);
+            -ms-transform: rotate(-90deg);
+            -o-transform: rotate(-90deg);
+            transform: rotate(-90deg);
         }
     }
 </style>
--- src/components/demo/progress/progress.vue
+++ src/components/demo/progress/progress.vue
@@ -2,18 +2,27 @@
 <template>
     <div class="section">
         <div class="main">
-            <h2 class="section-label">Progress</h2>
+            <h2 class="section-label">Progress 进度条</h2>
             <h4>基础用法</h4>
             <div class="demo-block">
                 <v-progress :percentage="50"></v-progress>
                 <v-progress :percentage="100" strokeColor="#3385ff"></v-progress>
-                <v-progress :percentage="70" :showVal="false"></v-progress>
+                <v-progress :percentage="70" :showText="false"></v-progress>
             </div>
             <h4>circle</h4>
             <div class="demo-block">
-                <v-progress :percentage="50" type="circle"></v-progress>
+                <v-progress :percentage="50" strokeColor="#ffc104" type="circle"></v-progress>
             </div>
-           
+             <h4>数据逻辑</h4>
+            <div class="demo-block">
+                <button @click="increase">    
+                    +
+                </button>
+                <button @click="decline">    
+                    -
+                </button>
+                <v-progress :percentage="60" :step="5" strokeColor="#3385ff"  ref="child1"></v-progress>
+            </div>
         </div>
     </div>
 </template>
@@ -26,12 +35,15 @@
         },
         data() {
             return {
-                progress: 2,
-                progress2: 2
             }
         },
         methods: {
-         
+         increase(){
+              this.$refs.child1.handleIncrease();
+         },
+         decline(){
+              this.$refs.child1.handleDecline();
+         }
         }
     }
 </script>
